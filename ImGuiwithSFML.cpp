b5fe511bb0734105e8120e77203fedb886ce3a3f
alans <alans@TABLET-1ODCITVC>
2022-12-19 10:34:43 +0000

    Add project files.

---
+++ ImGuiwithSFML.cpp
@@ -0,0 +1,47 @@
+// ImGuiwithSFML.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Player.h"
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include "SFML/Graphics.hpp"
+#define winWidth 800
+#define winHeight 600
+
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(winWidth, winHeight), "Test");
+    //window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    
+    sf::Texture txt_dwarve;
+    txt_dwarve.loadFromFile("dwarves.png");
+
+    Player player(&txt_dwarve, sf::Vector2u(16, 3), 0.2f, 100.0f);
+
+    float deltaTime = 0.0f;
+    sf::Clock clock;
+
+    while (window.isOpen()) {
+        deltaTime = clock.restart().asSeconds();
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+        }
+
+        player.Update(deltaTime);
+
+            window.clear();
+           
+            player.Draw(window);
+            window.display();
+    }
+}
+
